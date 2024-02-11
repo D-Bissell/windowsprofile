ed43f5a83ce1f8a0d138ed50107f65caa895178c
D-Bissell <david@bissell.id.au>
2024-02-12 09:40:34 +1100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,19 +1,15 @@
 # Run this script to setup windows profile
 
-$centralProfilePath = '$HOME\Documents\PsProfileConfig.ps1'
-
 # Install modules
 winget install JanDeDobbeleer.OhMyPosh
 install-module posh-git
 
 # Copy profile script to 'central' user location
-copy-item PsProfileConfig.ps1 $centralProfilePath
+copy-item PsProfileConfig.ps1 "$HOME\Documents\PsProfileConfig.ps1"
 
 
 # Add redirect to the system profile script
 # This may need to be done for both powershell 5 and powershell 7+
-Set-Content $PROFILE.CurrentUserAllHosts ". $centralProfilePath"
+Set-Content $PROFILE.CurrentUserAllHosts ". $HOME\Documents\PsProfileConfig.ps1"
 
 # then re-run profile .$PROFILE
-
-
