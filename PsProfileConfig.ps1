bc7ad727c7ddd9dd18412f184021b19289b46dd5
D-Bissell <david@bissell.id.au>
2024-02-12 09:12:33 +1100

    Add profile script

---
+++ PsProfileConfig.ps1
@@ -0,0 +1,25 @@
+# Powershell profile configuration.
+# Dot source this script from profile script $PROFILE.CurrentUserAllHosts for powershell 7 and 5
+
+# 
+## . $HOME\Documents\PsProfileConfig.ps1
+
+# Add post-git
+Import-Module posh-git
+
+
+# Add oh-my-posh and set the theme
+oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/gruvbox.omp.json" | Invoke-Expression
+
+
+
+#Cloud native azure good but too much stuff
+# Gruvbox
+
+#I want
+# path
+# NOT user
+# Git branch
+# Git status
+# azure sub on the right
