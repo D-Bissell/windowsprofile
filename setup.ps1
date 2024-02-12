866d0bd3cae80ab4274a5eb14a97f3c630879ce1
D-Bissell <david@bissell.id.au>
2024-02-12 11:46:19 +1100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -8,6 +8,19 @@ Install-module -Name posh-git
 Install-Module -Name Terminal-Icons -Repository PSGallery
 Install-Module -Name PSReadLine
 
+# Install Nerd font
+$FontName = 'CascadiaCode'
+$NerdFontsURI = 'https://github.com/ryanoasis/nerd-fonts/releases'
+Invoke-WebRequest -Uri "$NerdFontsURI/latest" -MaximumRedirection 0 -ErrorVariable err
+$LatestVersion = Split-Path -Path $err.InnerException.Response.Headers.Location -Leaf
+Invoke-WebRequest -Uri "$NerdFontsURI/download/$LatestVersion/$FontName.zip" -OutFile "$FontName.zip"
+Expand-Archive -Path "$FontName.zip"
+$ShellApplication = New-Object -ComObject shell.application
+$Fonts = $ShellApplication.NameSpace(0x14)
+Get-ChildItem -Path ".\$FontName" -Include '*.ttf' -Recurse | ForEach-Object -Process {
+    $Fonts.CopyHere($_.FullName)
+}
+
 # Copy profile script to 'central' user location
 copy-item PsProfileConfig.ps1 "$HOME\Documents\PsProfileConfig.ps1"
 
@@ -17,3 +30,5 @@ copy-item PsProfileConfig.ps1 "$HOME\Documents\PsProfileConfig.ps1"
 Set-Content $PROFILE.CurrentUserAllHosts ". $HOME\Documents\PsProfileConfig.ps1"
 
 # then re-run profile .$PROFILE
+
+
